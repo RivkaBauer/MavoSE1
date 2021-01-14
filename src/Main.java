ffc75e2a4d147101b65d0e7e61640f70f8ecfdce
RivkaBauer <rivka990@gmail.com>
2021-01-14 13:44:12 +0200

    1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import elements.AmbientLight;
+import elements.Camera;
+import elements.DirectionalLight;
+import elements.SpotLight;
+import geometries.*;
+//import org.junit.Assert;
+import primitives.*;
+import renderer.ImageWriter;
+import renderer.Render;
+import scene.Scene;
+
+import java.util.List;
+
+import static java.lang.System.out;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static primitives.Util.*;
+
+/**
+ * Test program for the 1st stage
+ *
+ * @author Dan Zilberstein
+ */
+public final class Main {
+
+    /**
+     * Main program to tests initial functionality of the 1st stage
+     *
+     * @param args irrelevant here
+     */
+    public static void main(String[] args) {
+
+    }
+}
